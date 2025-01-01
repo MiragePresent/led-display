110d649e45bf13a0d12d2d1f25401e56260f805e
Davyd Holovii <mirage.present@gmail.com>
2025-01-01 10:06:07 -0500

    poc display

---
+++ poc/index.js
@@ -0,0 +1,25 @@
+window.onload = function (e) { 
+    console.log("Initializing LED display POC...")
+    const display = document.getElementById("display")
+
+    createGrid(display, 43, 14)
+}
+
+function createGrid(display, width, height) {
+    console.log("Creating display: " + width + " x " + height)
+    const table = document.createElement("table")
+    table.setAttribute("id", "display-table")
+    display.appendChild(table)
+
+    for (let y = 0; y < height; y++) {
+        let tr = document.createElement("tr")
+        tr.setAttribute("id", "row-" + y)
+        table.appendChild(tr)
+
+        for (let x = 0; x < width; x++) {
+            let td = document.createElement("td")
+            td.setAttribute("id", "cell-" + x + "-" + y)
+            tr.appendChild(td)
+        }
+    }
+}
