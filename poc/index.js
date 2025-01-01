8e8285896e97c8e4b1c2018ebbe057e0601219c9
Davyd Holovii <mirage.present@gmail.com>
2025-01-01 12:08:35 -0500

    poc - doesn't support display limitations

--- poc/index.js
+++ poc/index.js
@@ -5,15 +5,29 @@ window.onload = function (e) {
     const textInput = document.getElementById("displaytext")
     const widthInput = document.getElementById("displaywidth")
     const heightInput = document.getElementById("displayheight")
+    const fontSize = document.getElementById("fontsize")
+    const bold = document.getElementById("fontbold")
 
     widthInput.onchange = function (e) {
         reCreateGrid(display, widthInput.value, heightInput.value)
     }
     heightInput.onchange = function (e) {
         reCreateGrid(display, widthInput.value, heightInput.value)
+        renderText(textInput.value, fontSize.value)
+    }
+    textInput.onchange = function (e) {
+        renderText(e.target.value, fontSize.value)
+        renderText(textInput.value, fontSize.value)
+    }
+    fontSize.onchange = function (e) {
+        if (e.target.value > 9) {
+            bold.setAttribute("disabled", "false")
+        }
+        renderText(textInput.value, fontSize.value)
     }
 
     createGrid(display, widthInput.value, heightInput.value)
+    renderText(textInput.value, fontSize.value)
 }
 
 function createGrid(display, width, height) {
@@ -29,8 +43,8 @@ function createGrid(display, width, height) {
 
         for (let x = 0; x < width; x++) {
             let td = document.createElement("td")
-            td.setAttribute("id", "cell-" + x + "-" + y)
-            td.setAttribute("class", "led-point")
+            td.setAttribute("id", "led-" + x + "-" + y)
+            td.setAttribute("class", "led-point off")
             tr.appendChild(td)
         }
     }
@@ -43,4 +57,147 @@ function reCreateGrid(display, width, height) {
         display.removeChild(table)
     }
     createGrid(display, width, height)
+}
+
+function turnOnLed(x,y) {
+    const led = document.getElementById(`led-${x}-${y}`)
+
+    if (led === undefined) {
+        console.log(`Warning! Cannot find LED ${x}:${y} (x:y)`)
+    }
+
+    led.setAttribute("class", "led-point on")
+}
+
+function turnOffLed(x,y) {
+    const led = document.getElementById(`led-${x}-${y}`)
+
+    if (led === undefined) {
+        console.log(`Warning! Cannot find LED ${x}:${y} (x:y)`)
+    }
+
+    led.setAttribute("class", "led-point off")
+}
+
+function renderText(text, fontSize, width, height) {
+    fontSize = parseInt(fontSize)
+    const abc = {
+        "7": {
+            "e": [
+                [1,1,1,1,1],
+                [1,0,0,0,0],
+                [1,0,0,0,0],
+                [1,1,1,1,1],
+                [1,0,0,0,0],
+                [1,0,0,0,0],
+                [1,1,1,1,1],
+            ],
+            "h": [
+                [1,0,0,0,1],
+                [1,0,0,0,1],
+                [1,0,0,0,1],
+                [1,1,1,1,1],
+                [1,0,0,0,1],
+                [1,0,0,0,1],
+                [1,0,0,0,1],
+            ],
+            "l": [
+                [1,0,0,0],
+                [1,0,0,0],
+                [1,0,0,0],
+                [1,0,0,0],
+                [1,0,0,0],
+                [1,0,0,0],
+                [1,1,1,1],
+            ],
+            "o": [
+                [0,1,1,1,0],
+                [1,0,0,0,1],
+                [1,0,0,0,1],
+                [1,0,0,0,1],
+                [1,0,0,0,1],
+                [1,0,0,0,1],
+                [0,1,1,1,0],
+            ],
+            " ": [[0],[0],[0],[0],[0],[0],[0],],
+            "space": [[0],[0],[0],[0],[0],[0],[0],],
+            "unknown": [
+                [1,1,1,1,1],
+                [1,0,0,1,1],
+                [1,0,0,1,1],
+                [1,0,1,0,1],
+                [1,1,0,0,1],
+                [1,1,0,0,1],
+                [1,1,1,1,1],
+            ],
+        },
+        "9": {
+            "h": [
+                [1,0,0,0,0,1],
+                [1,0,0,0,0,1],
+                [1,0,0,0,0,1],
+                [1,0,0,0,0,1],
+                [1,1,1,1,1,1],
+                [1,0,0,0,0,1],
+                [1,0,0,0,0,1],
+                [1,0,0,0,0,1],
+                [1,0,0,0,0,1],
+            ],
+            " ": [[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],],
+            "unknown": [
+                [1,1,1,1,1,1],
+                [1,0,0,0,1,1],
+                [1,0,0,1,0,1],
+                [1,0,0,1,0,1],
+                [1,0,1,1,0,1],
+                [1,0,1,0,0,1],
+                [1,0,1,0,0,1],
+                [1,1,0,0,0,1],
+                [1,1,1,1,1,1],
+            ]
+        }
+    }
+
+    if (fontSize < 7) {
+        fontSize = 7
+    }
+    let breaker = 0
+    while (abc[fontSize] === undefined && breaker < 100) {
+        console.log("Unsupported font size " + fontSize + ". Trying " + (--fontSize))
+        breaker++
+    }
+    if (abc[fontSize] === undefined) {
+        fontSize = 7
+        console.log("Unsupported font size. Defaulting to 7px")
+    }
+
+    // Print line by line
+    for (let y = 0; y < fontSize; y++) {
+        let x = 0;
+        for (let i = 0; i < text.length; i++) {
+            let c = text.charAt(i)
+            let char = abc[fontSize]["unknown"];
+            if (abc[fontSize][c] === undefined) {
+                console.log("Unknown character " + c)
+            } else {
+                char = abc[fontSize][c]
+            }
+
+            let charRow = char[y]
+
+            for (let j = 0; j < charRow.length; j++) {
+                let ledOn = charRow[j]
+                // console.log(`char ${c}, row ${y}, on: ${ledOn}`)
+                if (ledOn === 1) {
+                    turnOnLed(x,y)
+                } else {
+                    turnOffLed(x,y)
+                }
+                x++
+            }
+            turnOffLed(x,y)
+            x++ // space between letters
+        }
+    }
+
 }
